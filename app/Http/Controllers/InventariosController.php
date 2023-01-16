2b2490aaeba8f3bf5ec4fb2977e6664d2105fcc8
LeeDaeMin <laalmeida@uts.edu.co>
2023-01-16 14:40:54 -0500

    Add model and migration to DB (Inventarios)

--- app/Http/Controllers/InventariosController.php
+++ app/Http/Controllers/InventariosController.php
@@ -19,6 +19,7 @@ class InventariosController extends Controller
     }
 
     /**
+        
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
---
+++ database/migrations/2023_01_16_160309_create_inventarios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventarios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_Bodega');
+            $table->unsignedBigInteger('id_Producto');
+            $table->timestamps();
+
+            $table->foreign('id_Bodega')->references('id')
+                ->on('bodegas');
+
+            $table->foreign('id_Producto')->references('id')
+                ->on('productos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventarios');
+    }
+};
